00b8e34a26683590e7303421c743360f088da1a4
3bdulra7manAmir <ethanhunt4d@gmail.com>
2025-09-28 12:54:43 +0300

    update

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -3,7 +3,7 @@ import logo from "../../../assets/logo.png";
 /* Footer nabLinks */
 const navItems = [
   { id: 1, name: "Home", url: "Home" },
-  { id: 2, name: "About", url: "About" },
+  { id: 2, name: "About", url: "profile" },
   { id: 3, name: "Process", url: "work-process" },
   { id: 4, name: "Portfolio", url: "Portfolio" },
   { id: 5, name: "Blog", url: "Blog" },
